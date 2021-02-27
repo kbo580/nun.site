2febc3db4ee0b62beaaa43e63ace7f6c983a3098
kbo580 <61081989+kbo580@users.noreply.github.com>
2021-02-27 22:48:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-// 'use strict';
-// {
-//   const worksName = ['kboA','kboB','kboC'];
-//   const charaName = ['k','b','o'];
-//   const explain = ['すごい','いけてる','かわいい'];
+'use strict';
+{
+  //データベース//
+//   const galleryExplaination = [
+//     {name: 'aaa',chara: 'bbb',explain: 'こんなキャラです。'},
+//     {name: 'ccc',chara: 'ddd',explain: 'こんなキャラです。こんなキャラです。こんなキャラです。'},
+//     {name: 'eee',chara: 'fff',explain: 'こんなキャラです。こんなキャラです。こんなキャラです。こんなキャラです。'},
+// ];
 //   const worksName = document.getElementById('worksName');
 //   const charaName = document.getElementById('charaName');
 //   const explain = document.getElementById('explain');
-//   const pcThumb = document.querySelectorAll('pc-gallery-thumbnail');
-  
-//   console.log(`${worksName[0]}は${explain[1]}`)
-//   console.log(worksName)
-  
-//   worksName.addEventListener('click', ()=>{
-//     worksName.textContent = 'kbo';
-//     charaName.textContent = 'kbo';
-//     explain.textContent = 'kbo';
-//   })
 
-// }
+
+
+  
+
+
+
+
+  // const kbo = document.getElementById('kbo');
+  // kbo.addEventListener('click', ()=>{
+  //   worksName.textContent = galleryExplaination[0].name;
+  //   charaName.textContent = galleryExplaination[0].chara;
+  //   explain.textContent = galleryExplaination[0].explain;
+  // });
+
+  // const kbo2 = document.getElementById('kbo2');
+  // kbo2.addEventListener('click', ()=>{
+  //   worksName.textContent = galleryExplaination[1].name;
+  //   charaName.textContent = galleryExplaination[1].chara;
+  //   explain.textContent = galleryExplaination[1].explain;
+  // });
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
@@ -29,47 +52,64 @@ $(function(){
 $('.header-nav a').click(function(){
     var id =$(this).attr('href');
     var position = $(id).offset().top;
-    $('html, body').animate({'scrollTop(200px)': position}, 300);
+    $('html, body').animate({'scrollTop': position}, 300);
   });
 
-
-//サムネをクリックするとギャラリーの画像が変化する
-  $('.sp-gallery-thumbnail').click(function(){
-    $('.slideA').removeClass('active');
-    $('.slideB').removeClass('active');
-    var index = $('.sp-gallery-thumbnail').index($(this));
-    $('.slideA').eq(index).addClass('active');
-    $('.slideB').eq(index).addClass('active');
+  //サムネクリックで拡大
+  $('.work-thumbnail').click(function() {
+    var imgSrc = $(this).children().attr('src');
+    $('#big').children().attr('src', imgSrc);
+    $('.modal').fadeIn();
+    $('body,html').css('overflow-y', 'hidden');
   });
-
-  $('.pc-gallery-thumbnail').click(function(){
-    $('.slideA').removeClass('active');
-    $('.slideB').removeClass('active');
-    var index = $('.pc-gallery-thumbnail').index($(this));
-    $('.slideA').eq(index).addClass('active');
-    $('.slideB').eq(index).addClass('active');
+  
+  //closeボタンで閉じる
+  $('#close').click(function() {
+    $('.modal').fadeOut();
+    $('body,html').css('overflow-y', 'visible');
   });
+  // $('.modal').click(function(){
+  //   $('#close').click();
+  // });
 
-//サムネをクリックするとギャラリーの文字が変化する
-// $('.pc-gallery-thumbnail').click(function(){
-//     $('#worksName').text('kbo');
+
+////////////////////
+//ギャラリページ用//
+////////////////////
+// サムネをクリックするとギャラリーの文字が変化する
+// $('#kbo').click(function(){
+//     $('#worksName').text('nameA');
+//     $('#charaName').text('nameA2');
+//     $('#explain').text('こんなキャラです。こんなキャラです。');
 // });
 
+// $('#kbo2').click(function(){
+//     $('#worksName').text('nameB');
+//     $('#charaName').text('nameB2');
+//     $('#explain').text('こんなキャラです。こんなキャラです。こんなキャラです。こんなキャラです。');
+// });
 
-$('.work-thumbnail').click(function() {
+//スマホ、アンドロイドの時にサムネをクリックするとギャラリーの画像が変化する
+$('.sp-gallery-thumbnail').click(function() {
   var imgSrc = $(this).children().attr('src');
-  $('#big').children().attr('src', imgSrc);
-  $('.modal').fadeIn();
-  $('body,html').css('overflow-y', 'hidden');
+  $('.gallery-left li').children().attr('src', imgSrc);
+
+  $('.gallery-right-image').removeClass('active');
+  var index = $('.sp-gallery-thumbnail').index($(this));
+  $('.gallery-right-image').eq(index).addClass('active');
 });
 
-$('#close').click(function() {
-  $('.modal').fadeOut();
-  $('body,html').css('overflow-y', 'visible');
-});
-// $('.modal').click(function(){
-//   $('#close').click();
+// pcの時にサムネをクリックするとギャラリーの画像が変化する
+// $('.pc-gallery-thumbnail').click(function() {
+//   var imgSrc = $(this).children().attr('src');
+//   $('.gallery-left li').children().attr('src', imgSrc);
+
+//   $('.gallery-right-image').removeClass('active');
+//   var index = $('.pc-gallery-thumbnail').index($(this));
+//   $('.gallery-right-image').eq(index).addClass('active');
 // });
+
+
 
 
 
