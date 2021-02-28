820fa5aeec4f7f79645b848177b3d7990f452c97
kbo580 <61081989+kbo580@users.noreply.github.com>
2021-03-01 00:15:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,53 +1,4 @@
-'use strict';
-{
-  //データベース//
-//   const galleryExplaination = [
-//     {name: 'aaa',chara: 'bbb',explain: 'こんなキャラです。'},
-//     {name: 'ccc',chara: 'ddd',explain: 'こんなキャラです。こんなキャラです。こんなキャラです。'},
-//     {name: 'eee',chara: 'fff',explain: 'こんなキャラです。こんなキャラです。こんなキャラです。こんなキャラです。'},
-// ];
-//   const worksName = document.getElementById('worksName');
-//   const charaName = document.getElementById('charaName');
-//   const explain = document.getElementById('explain');
-
-
-
-  
-
-
-
-
-  // const kbo = document.getElementById('kbo');
-  // kbo.addEventListener('click', ()=>{
-  //   worksName.textContent = galleryExplaination[0].name;
-  //   charaName.textContent = galleryExplaination[0].chara;
-  //   explain.textContent = galleryExplaination[0].explain;
-  // });
-
-  // const kbo2 = document.getElementById('kbo2');
-  // kbo2.addEventListener('click', ()=>{
-  //   worksName.textContent = galleryExplaination[1].name;
-  //   charaName.textContent = galleryExplaination[1].chara;
-  //   explain.textContent = galleryExplaination[1].explain;
-  // });
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
 $(function(){
-
 //スクロール
 $('.header-nav a').click(function(){
     var id =$(this).attr('href');
@@ -77,37 +28,37 @@ $('.header-nav a').click(function(){
 //ギャラリページ用//
 ////////////////////
 // サムネをクリックするとギャラリーの文字が変化する
-// $('#kbo').click(function(){
-//     $('#worksName').text('nameA');
-//     $('#charaName').text('nameA2');
-//     $('#explain').text('こんなキャラです。こんなキャラです。');
-// });
 
-// $('#kbo2').click(function(){
-//     $('#worksName').text('nameB');
-//     $('#charaName').text('nameB2');
-//     $('#explain').text('こんなキャラです。こんなキャラです。こんなキャラです。こんなキャラです。');
-// });
 
 //スマホ、アンドロイドの時にサムネをクリックするとギャラリーの画像が変化する
-$('.sp-gallery-thumbnail').click(function() {
+$('.gallery-thumbnail').click(function() {
   var imgSrc = $(this).children().attr('src');
   $('.gallery-left li').children().attr('src', imgSrc);
 
   $('.gallery-right-image').removeClass('active');
-  var index = $('.sp-gallery-thumbnail').index($(this));
+  var index = $('.gallery-thumbnail').index($(this));
   $('.gallery-right-image').eq(index).addClass('active');
 });
 
-// pcの時にサムネをクリックするとギャラリーの画像が変化する
-// $('.pc-gallery-thumbnail').click(function() {
-//   var imgSrc = $(this).children().attr('src');
-//   $('.gallery-left li').children().attr('src', imgSrc);
+var db = [
+  {name: 'exampleA', chara: 'exampleA', explain: '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明'},
+  {name: 'exampleB', chara: 'exampleB', explain: '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明'},
+  {name: 'exampleC', chara: 'exampleC', explain: '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明'},
+];
 
-//   $('.gallery-right-image').removeClass('active');
-//   var index = $('.pc-gallery-thumbnail').index($(this));
-//   $('.gallery-right-image').eq(index).addClass('active');
-// });
+$('.gallery-thumbnail').click(function() {
+  var thumbIndex = $('.gallery-thumbnail').index($(this));
+  $('#worksName').text(db[thumbIndex].name);
+  $('#charaName').text(db[thumbIndex].chara);
+  $('#explain').text(db[thumbIndex].explain);
+});
+
+
+
+
+
+
+
 
 
 
